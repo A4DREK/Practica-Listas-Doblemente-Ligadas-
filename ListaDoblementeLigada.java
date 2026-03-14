fd078ccd4fd731e5f7cf28b96d2ac92047c45b4a
A4DREK <yael.adrian640305@gmail.com>
2026-03-14 13:29:21 -0600

    Agregado de ambos métodos de agregar

--- ListaDoblementeLigada.java
+++ ListaDoblementeLigada.java
@@ -3,15 +3,63 @@ public class ListaDoblementeLigada<T> {
   private Nodo<T> rabo;
   private int elementos; //Este se refiere al tamaño de la lista
 
-  public void add(int indice, T elementos){
+  public boolean isEmpty(){
+    return this.cabeza == null && this.rabo == null;
+  }
+
+  public void add(int indice, T elemento){
     if(indice < 0 || indice > this.elementos){
       throw new IndexOutOfBoundsException("Índice fuera del rango permitido");
     }
+
+    Nodo<T> nuevoNodo = new Nodo<>(elemento);
+    if(this.isEmpty()){
+      this.cabeza = nuevoNodo;
+      this.rabo = nuevoNodo;
+      nuevoNodo.setSiguiente(null);
+      nuevoNodo.setAnterior(null);
+      elementos++;
+      return;
+    }
+
+    if(indice == 0){
+      nuevoNodo.setSiguiente(cabeza);
+      cabeza.setAnterior(nuevoNodo);
+      nuevoNodo.setAnterior(null);
+      cabeza = nuevoNodo;
+      elementos++;
+      return;
+    }
+
+    if(indice == elementos){
+      nuevoNodo.setAnterior(rabo);
+      rabo.setSiguiente(nuevoNodo);
+      nuevoNodo.setSiguiente(null);
+      rabo = nuevoNodo;
+      elementos++;
+      return;
+    }
+    
+    Nodo<T> aux = cabeza;
+    for(int i = 0; i < elementos; i++){
+      aux = aux.getSiguiente();
+    }
+    nuevoNodo.setAnterior(aux.getAnterior());
+    nuevoNodo.setSiguiente(aux);
+    aux.getAnterior().setAnterior(nuevoNodo);
+    aux.setAnterior(nuevoNodo);
+    elementos++;
     return;
   }
 
   public boolean add(T elemento){
-    return true;
+    Nodo<T> nuevoNodo = new Nodo<>(elemento);
+    nuevoNodo.setAnterior(rabo);
+    rabo.setSiguiente(nuevoNodo);
+    nuevoNodo.setSiguiente(null);
+    rabo = nuevoNodo;
+    elementos++;
+    return this.contains(nuevoNodo);
   }
 
   public boolean contains(Nodo<T> objeto){
@@ -65,5 +113,19 @@ public class ListaDoblementeLigada<T> {
       }
     }
     return cadena + " null";
+  }
+
+  public static void main(String[] args){
+    ListaDoblementeLigada<Integer> lista = new ListaDoblementeLigada<>();
+    for(int i = 0; i < 10; i++){
+      lista.add(0, i+1);
+    }  
+    System.out.println(lista);
+
+    lista.add(99);
+    System.out.println(lista);
+
+    lista.add(11, 60);
+    System.out.println(lista);
   }
 }
